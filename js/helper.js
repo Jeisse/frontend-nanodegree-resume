51d99aabf8f6153037c0ab62d83420cc1b6cbf5a
Jeisse <jeisserocha@gmail.com>
2015-08-02 19:16:11 +0100

    Finished project

--- js/helper.js
+++ js/helper.js
@@ -13,7 +13,7 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr/>';
+var HTMLheaderRole = '<span>%data%</span>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
@@ -59,6 +59,7 @@ var HTMLonlineURL = '<br><a href="#">%data%</a>';
 var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
 
+var menuItem = "<li><a href='%link%'>%description%</a></li>";
 
 /*
 The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
@@ -86,7 +87,9 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+  var x = loc.pageX;
+  var y = loc.pageY;
+  console.log(logClicks(x,y));
 });
 
 
@@ -132,7 +135,7 @@ function initializeMap() {
     // iterates through school locations and appends each location to
     // the locations array
     for (var school in education.schools) {
-      locations.push(education.schools[school].location);
+      locations.push(education.schools[school].city);
     }
 
     // iterates through work locations and appends each location to
@@ -174,6 +177,8 @@ function initializeMap() {
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
       // your code goes here!
+      map.setZoom(8);
+      map.setCenter(marker.getPosition());
     });
 
     // this is where the pin actually gets added to the map.
@@ -235,12 +240,12 @@ function initializeMap() {
 Uncomment the code below when you're ready to implement a Google Map!
 */
 
-// Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+//Calls the initializeMap() function when the page loads
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+ map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,211 +1,252 @@
 
 var bio = {
-	"name" : "Jeisse Rocha",
-	"role" : "Web Developer",
-	"Contacts" : {
-		"email" : "jeisserocha@gmail.com",
-		"mobile" : "+353-83-1859999",
-		"gitHub" : "jeisse",
-		"location" : "Dublin"
-	},
-	"wellComeMessage" : "Obsessive thoroegh responsive web develpemnt, with HTML 5, CSS 3, jQuery and Bootstrap",
-	"skill" : ["HTML5", "CSS3", "JS", "jQuery", "Angular", "Java", "GIT", "PHP", "Scrum", "Analysis", "SQL"],
-	"myPicture" : "images/jr.png"
+    "name": "Jeisse Rocha",
+    "role": "Web Developer",
+    "contacts": {
+        "email": "jeisserocha@gmail.com",
+        "mobile": "+353-83-1859999",
+        "gitHub": "jeisse",
+        "location": "Dublin, IE"
+    },
+    "wellComeMessage": "Obsessive thoroegh responsive web develpemnt, with HTML 5, CSS 3, jQuery and Bootstrap",
+    "skill": [
+        "HTML5",
+        "CSS3",
+        "JS",
+        "jQuery",
+        "Angular",
+        "Java",
+        "GIT",
+        "PHP",
+        "Scrum",
+        "Analysis",
+        "SQL"
+    ],
+    "myPicture": "images/jr.png"
 }
 
 var work = {
-	"jobs" : [
-		{
-			"employer" : "Xetec LTDa",
-			"title" : "Software Developer",
-			"dates" : "Desember 2013 until now",
-			"location" : "Bray, IE",
-			"description" : 'Some description here'
-		},
-		{
-			"employer" : "Terra.com",
-			"title" : "Web Developer",
-			"dates" : "March 2012 until August 2013",
-			"location" : "Porto Alegre, Brazil",
-			"description" : 'Worked on several teams and different fronts, such as special pages for the London Olympics, and UX CMS migration standardization for Spain, calendar pages of sports, new pages of videos portal Terra.'
-		},
-		{
-			"employer" : "Procergs",
-			"title" : "Software Developer",
-			"dates" : "January 2012 until April 2012",
-			"location" : "Porto Alegre, Brazil",
-			"description" : 'Worked with Java EE and Oracle on a ERP.'
-		},
-		{
-			"employer" : "Cia Salux",
-			"title" : "Software Developer",
-			"dates" : "January 2011 until January 2012",
-			"location" : "Porto Alegre, Brazil",
-			"description" : 'Worked with Powerbuilder and Oracle on a ERP for Hospitals.'
-		},
-		{
-			"employer" : "Cia Star4",
-			"title" : "Software Developer",
-			"dates" : "June 2010 until January 2011",
-			"location" : "Taquara, RS, Brazil",
-			"description" : 'Worked with Javascript and Php on a ERP for the footwear market.'
-		}
-	]
-};
-
-
-var project ={
-	"projects" : [
-		{
-			"title" : "FlexMaint Software",
-			"dates" : 2014,
-			"description" : "A CMMS Software migrated fromn a desktop version to a new web based version.",
-			"image" : "images/flexmaint.png"
-		},
-		{
-			"title" : "TerraTv",
-			"dates" : 2013,
-			"description" : "New portal to show all videos related to the...",
-			"image" : "images/terratv.jpg"
-		},
-		{
-			"title" : "Easilocks WebSite",
-			"dates" : 2015,
-			"description" : "Easilocks WebSite is to know moe about the brand, purchase products, buy training, find physical stores all over the world and some more functions.",
-			"image" : "images/easilocks.png"
-		}
-	]
-};
-  
-var education = {
-	"schools" : [
-		{
-			"name" : "FATEC - Senac RS",
-			"degree" : "Higher Level",
-			"dates" : "2013",
-			"city" : "Porto Alegre - RS, Brazil",
-			"major" : "Ver o que colocar aqui"
-		}
-		],
-	"onlineCourses" : [
-		{
-			"title" : "Front-End Web Developer Nanodeegree",
-			"school" : "Nanodeegree",
-			"dates" : "2015",
-			"url" : "http://"
-		},
-		{
-			"title" : "Learn how to learn",
-			"school" : "Coursera",
-			"dates" : 2015,
-			"url" : "http://"
-		}
-	]
-};
-
-function setBioInformation(place) {
-	$(place).prepend(fomatedEmail);  
-	$(place).prepend(fomatedMobile);  
-	$(place).prepend(fomatedGitHub);  
-	$(place).prepend(fomatedLocation);
+    "jobs": [
+        {
+            "employer": "Xetec LTDa",
+            "title": "Software Developer",
+            "dates": "Desember 2013 until now",
+            "location": "Bray, IE",
+            "description": "Work as a Back-End and Front-End on a small team, doing several taks."
+        },
+        {
+            "employer": "Terra.com",
+            "title": "Web Developer",
+            "dates": "March 2012 until August 2013",
+            "location": "Porto Alegre, RS, Brazil",
+            "description": "Worked on several teams and different fronts, such as special pages for the London Olympics, and UX CMS migration standardization for Spain, calendar pages of sports, new pages of videos portal Terra."
+        },
+        {
+            "employer": "Procergs",
+            "title": "Software Developer",
+            "dates": "January 2012 until April 2012",
+            "location": "Porto Alegre, RS, Brazil",
+            "description": "Worked with Java EE and Oracle on a ERP."
+        },
+        {
+            "employer": "Cia Salux",
+            "title": "Software Developer",
+            "dates": "January 2011 until January 2012",
+            "location": "Porto Alegre, RS, Brazil",
+            "description": "Worked with Powerbuilder and Oracle on a ERP for Hospitals."
+        },
+        {
+            "employer": "Cia Star4",
+            "title": "Software Developer",
+            "dates": "June 2010 until January 2011",
+            "location": "Taquara, RS, Brazil",
+            "description": "Worked with Javascript and Php on a ERP for the footwear market."
+        }
+    ]
 }
 
-function setSkillsList(bio){
-	var fomatedSkills = [];
+var project = {
+    "projects": [
+        {
+            "title": "FlexMaint Software",
+            "dates": 2014,
+            "description": "A CMMS Software migrated fromn a desktop version to a new web based version.",
+            "image": "images/flexmaint.png"
+        },
+        {
+            "title": "TerraTv",
+            "dates": 2013,
+            "description": "Terra Networks, S.A. is a Spanish Internet multinational company with headquarters in Spain and Brazil.",
+            "image": "images/terratv.jpg"
+        },
+        {
+            "title": "Easilocks WebSite",
+            "dates": 2015,
+            "description": "Easilocks WebSite is to know more about the brand, purchase products, buy training, find physical stores.",
+            "image": "images/easilocks.png"
+        }
+    ]
+}
+  
+var education = {
+    "schools": [
+        {
+            "name": "FATEC - Senac RS",
+            "degree": "Higher Level",
+            "dates": "2013",
+            "city": "Porto Alegre, RS, Brazil",
+            "major": "Ver o que colocar aqui"
+        }
+    ],
+    "onlineCourses": [
+        {
+            "title": "Front-End Web Developer Nanodeegree",
+            "school": "Nanodeegree",
+            "dates": "2015",
+            "url": "https://www.udacity.com"
+        },
+        {
+            "title": "Learn how to learn",
+            "school": "Coursera",
+            "dates": 2015,
+            "url": "http://www.coursera.com"
+        }
+    ]
+}
 
-	for(i=0; i < bio.skill.length; i++ ){
-		var skill = HTMLskills.replace("%data%", bio.skill[i]);
-		fomatedSkills.push( skill);
-	}
-	return fomatedSkills;
+var menu = {
+    "items": [
+        {
+            "description": "Home",
+            "link": "#header"
+        },
+        {
+            "description": "Work Experience",
+            "link": "#workExperience"
+        },
+        {
+            "description": "Projects",
+            "link": "#projects"
+        },
+        {
+            "description": "Education",
+            "link": "#education"
+        },
+        {
+            "description": "Map",
+            "link": "#mapDiv"
+        }
+    ]
+}
+
+function setBioInformation(place) {
+  $(place).prepend(fomatedEmail);  
+  $(place).prepend(fomatedMobile);  
+  $(place).prepend(fomatedGitHub);  
+  $(place).prepend(fomatedLocation);
+}
+
+function setSkillsList(){
+  var fomatedSkills = [];
+
+  for(i=0; i < bio.skill.length; i++ ){
+    var skill = HTMLskills.replace("%data%", bio.skill[i]);
+    fomatedSkills.push( skill);
+  }
+  return fomatedSkills;
 }
 
 function setWorkJobs(work, place){
-	
-	for(job in work.jobs ){
-		var workEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-		var workTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-		var workDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-		var workLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
-		var workDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+  for(job in work.jobs ){
+    var workEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+    var workTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+    var workDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+    var workLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+    var workDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
 
-		$(place).append(workEmployer);
-		$(place).append(workTitle);
-		$(place).append(workDates);
-		$(place).append(workLocation);
-		$(place).append(workDescription);
-	}
+    $(place).append(workEmployer);
+    $(place).append(workTitle);
+    $(place).append(workDates);
+    $(place).append(workLocation);
+    $(place).append(workDescription);
+  }
 }
 
 function setProject(project, place){
-	
-	for(proj in project.projects ){
-		var projectTitle = HTMLprojectTitle.replace("%data%", project.projects[proj].title);
-		var projectDates = HTMLprojectDates.replace("%data%", project.projects[proj].dates);
-		var projectDescription = HTMLprojectDescription.replace("%data%", project.projects[proj].description);
-		var projectImage = HTMLprojectImage.replace("%data%", project.projects[proj].image);
+  for(proj in project.projects ){
+    var projectTitle = HTMLprojectTitle.replace("%data%", project.projects[proj].title);
+    var projectDates = HTMLprojectDates.replace("%data%", project.projects[proj].dates);
+    var projectImage = HTMLprojectImage.replace("%data%", project.projects[proj].image);
+    var projectDescription = HTMLprojectDescription.replace("%data%", project.projects[proj].description);
 
-		$(place).append( "<div id='proj"+proj+"' class='project'></div>");
-		$("#proj"+proj).append(projectTitle);
-		$("#proj"+proj).append(projectDates);
-		$("#proj"+proj).append(projectDescription);
-		$("#proj"+proj).append(projectImage);
-	}
+    $(place).append( "<div id='proj"+proj+"' class='project'></div>");
+    $("#proj"+proj).append(projectTitle);
+    $("#proj"+proj).append(projectDates);
+    $("#proj"+proj).append(projectDescription);
+    $("#proj"+proj).append(projectImage);
+  }
 }
 
-function setSchools(education, place){
-	
-	for(school in education.schools){
-		var schoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
-		var schoolDegree = HTMLschoolDegree.replace("%data%",  education.schools[school].degree);
-		var schoolDates = HTMLschoolDates.replace("%data%",  education.schools[school].dates);
-		var schoolLocation = HTMLschoolLocation.replace("%data%",  education.schools[school].location);
-		var schoolMajor = HTMLschoolMajor.replace("%data%",  education.schools[school].major);
+function setSchools(place){	
+  for(school in education.schools){
+    var schoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
+    var schoolDegree = HTMLschoolDegree.replace("%data%",  education.schools[school].degree);
+    var schoolDates = HTMLschoolDates.replace("%data%",  education.schools[school].dates);
+    var schoolLocation = HTMLschoolLocation.replace("%data%",  education.schools[school].city);
+    var schoolMajor = HTMLschoolMajor.replace("%data%",  education.schools[school].major);
  
-		$(place).append(schoolName);
-		$(place).append(schoolDegree);
-		$(place).append(schoolDates);
-		$(place).append(schoolLocation);
-		$(place).append(schoolMajor);
-	}
+    $(place).append(schoolName);
+    $(place).append(schoolDegree);
+	$(place).append(schoolDates);
+    $(place).append(schoolLocation);
+    $(place).append(schoolMajor);
+  }
 }
 
-function setOnlineCourses(education, place){
-	
-	for(onlineC in education.onlineCourses){
-		var onlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[onlineC].title);
-		var onlineSchool = HTMLonlineSchool.replace("%data%",  education.onlineCourses[onlineC].school);
-		var onlineDates = HTMLonlineDates.replace("%data%",  education.onlineCourses[onlineC].dates);
-		var onlineURL = HTMLonlineURL.replace("%data%",  education.onlineCourses[onlineC].url);
+function setOnlineCourses(place){
+  for(onlineC in education.onlineCourses){
+    var onlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[onlineC].title);
+    var onlineSchool = HTMLonlineSchool.replace("%data%",  education.onlineCourses[onlineC].school);
+    var onlineDates = HTMLonlineDates.replace("%data%",  education.onlineCourses[onlineC].dates);
+    var onlineURL = HTMLonlineURL.replace("%data%",  education.onlineCourses[onlineC].url);
 
-		$(place).append(onlineTitle);
-		$(place).append(onlineSchool);
-		$(place).append(onlineDates);
-		$(place).append(onlineURL);	
-	}
+    $(place).append(onlineTitle);
+    $(place).append(onlineSchool);
+    $(place).append(onlineDates);
+    $(place).append(onlineURL);	
+  }
 }
 
+function setMenu(){
+  for (i in menu.items){
+    var formatedItem = menuItem.replace('%link%', menu.items[i].link).replace('%description%', menu.items[i].description);
+
+    $(".container ul").append(formatedItem);
+  }
+}
 
 var formatedName = HTMLheaderName.replace("%data%", bio.name);
 var formatedRole = HTMLheaderRole.replace("%data%", bio.role);
 
-var fomatedEmail = HTMLemail.replace("%data%", bio.Contacts.email);
-var fomatedMobile = HTMLmobile.replace("%data%", bio.Contacts.mobile);
-var fomatedGitHub = HTMLgithub.replace("%data%", bio.Contacts.gitHub);
-var fomatedLocation = HTMLlocation.replace("%data%", bio.Contacts.location);
+var fomatedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+var fomatedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+var fomatedGitHub = HTMLgithub.replace("%data%", bio.contacts.gitHub);
+var fomatedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
 
 var formatedMessage = HTMLwelcomeMsg.replace("%data%", bio.wellComeMessage);
 var formatedPicture = HTMLbioPic.replace("%data%", bio.myPicture);
 
 
 $("#header").prepend(formatedPicture);
-$("#header").prepend(formatedRole);
-$("#header").prepend(formatedName);
+$(".logo").prepend(formatedRole);
+$(".logo").prepend(formatedName);
+
+setMenu();
 
 setBioInformation("#topContacts");
 
 $("#topContacts").after(HTMLskillsStart);
-$("#skills").append(setSkillsList(bio));
+$("#skills").append(setSkillsList());
 
 $("#topContacts").after(formatedMessage);
 
@@ -222,35 +263,10 @@ setProject(project, ".project-entry");
 $('div.project-entry img').css({'width' : '360px' , 'height' : '207px'});
 
 $("#education").append(HTMLschoolStart);
-setSchools(education, ".education-entry");
+setSchools(".education-entry");
 
-
-
-setOnlineCourses(education, ".education-entry");
 $("#education").append(HTMLonlineClasses);
+$("#education").append(HTMLschoolStart);
+setOnlineCourses(".education-entry:last");
 
-/**
-  
-    if(document.getElementsByClassName('flex-item').length === 0) {
-      document.getElementById('topContacts').style.display = 'none';
-    }
-    if(document.getElementsByTagName('h1').length === 0) {
-      document.getElementById('header').style.display = 'none';
-    }
-    if(document.getElementsByClassName('work-entry').length === 0) {
-      document.getElementById('workExperience').style.display = 'none';
-    }
-    if(document.getElementsByClassName('project-entry').length === 0) {
-      document.getElementById('projects').style.display = 'none';
-    }
-    if(document.getElementsByClassName('education-entry').length === 0) {
-      document.getElementById('education').style.display = 'none';
-    }
-    if(document.getElementsByClassName('flex-item').length === 0) {
-      document.getElementById('lets-connect').style.display = 'none';
-    }
-   if(document.getElementById('map') === null) {
-     document.getElementById('map-div').style.display = 'none';
-   }
-
- **/
+$("#mapDiv").append(googleMap);
